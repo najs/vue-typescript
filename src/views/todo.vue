9835d2a856bc2f4527f7ab1b051d2bd107b9c16c
joana <joa.na@cheilpengtai.com>
2020-05-08 14:54:25 +0900

    init

---
+++ src/views/todo.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="home">
+		<TodoList msg="!!TODO LIST!!"/>
+	</div>
+</template>
+
+<script>
+	// @ is an alias to /src
+	import TodoList from '@/components/todolist.vue'
+
+	export default {
+		name: 'Home',
+		components: {
+			TodoList
+		}
+	}
+</script>
